800d4c0ffc29bb93129df4ef30141d927b2652f2
guxttavo <gustavowbarbosas@gmail.com>
2023-09-20 23:20:44 -0300

    frontend home

--- Web/wwwroot/js/menu.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    // Seleciona todas as opções do menu
-    const opcoesMenu = $('.opcoes-lista');
-
-    // Função para lidar com o clique em uma opção do menu
-    opcoesMenu.click(function() {
-        $(this).addClass('selecionado');
-    });
-});
