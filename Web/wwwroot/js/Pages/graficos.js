d6c986f59338dc37ca4782f7d76f8541c336e1e5
guxttavo <gustavowbarbosas@gmail.com>
2023-09-23 02:19:56 -0300

    sei oq fiz n  kkkk

--- Web/wwwroot/js/Pages/graficos.js
+++ Web/wwwroot/js/Pages/graficos.js
@@ -1,7 +1,10 @@
-let el = document.getElementById("graficos")
+let grafico1 = document.getElementById("graficos")
 var options = {
     chart: {
-        type: 'bar'
+        type: 'bar',
+        height: '260px',
+        width: '322px',
+        margin: '100px'
     },
     series: [
         {
@@ -11,5 +14,64 @@ var options = {
     ]
 }
 
-var chart = new ApexCharts(el, options);
+var chart = new ApexCharts(grafico1, options);
+chart.render()
+
+let grafico2 = document.getElementById("graficos2")
+var options = {
+    chart: {
+        type: 'bar',
+        height: '260px',
+        width: '322px',
+        margin: '100px'
+    },
+    series: [
+        {
+            name: 'População',
+            data: [10, 30, 20]
+        }
+    ]
+}
+
+var chart = new ApexCharts(grafico2, options);
+chart.render()
+
+
+let grafico3 = document.getElementById("graficos3")
+var options = {
+    chart: {
+        type: 'bar',
+        height: '260px',
+        width: '322px',
+        margin: '100px'
+    },
+    series: [
+        {
+            name: 'População',
+            data: [10, 30, 20]
+        }
+    ]
+}
+
+var chart = new ApexCharts(grafico3, options);
+chart.render()
+
+
+let grafico4 = document.getElementById("graficos4")
+var options = {
+    chart: {
+        type: 'bar',
+        height: '260px',
+        width: '322px',
+        margin: '100px'
+    },
+    series: [
+        {
+            name: 'População',
+            data: [10, 30, 20]
+        }
+    ]
+}
+
+var chart = new ApexCharts(grafico4, options);
 chart.render()
