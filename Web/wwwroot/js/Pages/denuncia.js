30b0843b57de9f0980c967d182824c05c71f09a3
guxttavo <gustavowbarbosas@gmail.com>
2023-10-22 22:21:42 -0300

    parte da função jquery dos dropdowns

--- Web/wwwroot/js/Pages/denuncia.js
+++ Web/wwwroot/js/Pages/denuncia.js
@@ -10,13 +10,72 @@ var denuncia = (function () {
         configs = $configs;
     };
 
-    $(document).ready(function () {
-        if (categoria.id == categoria.IdCategoriaPai) {
+    // $(document).ready(function () {
+    //     var subcategorias = $("#subcategorias").val();
+    //     var categorias = $("#categorias");
+    //     console.log("JOOJ" + subcategorias);
 
-        }
-    })
+    //     subcategorias.empty();
+
+    //     $("#categorias").change(function () {
+    //         subcategorias.append('<option value="teste">teste</option>');
+    //     })
+    // })
+
+
+    $("#categorias").on('change', function () {
+        var idCategoria = $(this).val();
+        var idCategoriaPai = $("#subcategorias").val();
+        // var Subcategorias = $("#subcategorias");
+        var subCategorias = [];
+        // $("#subcategorias").empty();
+
+        $("#subcategorias option").each(function () {
+            var id = $(this).val();
+            var nome = $(this).text();
+            subCategorias.push({
+                id: id,
+                nome: nome
+            });
+
+        })
+
+        $.each(subCategorias, function () {
+            // if (idCategoria == idCategoriaPai) {
+            //     $("#subcategorias").append('<option value="' + idCategoriaPai + '">' + subCategorias.nome + '</option>');
+            // }
+            if (idCategoria == this.id) {
+                $("#macaco").empty();
+                $("#subcategorias").append('<option value="' + this.id + '">' + this.nome + '</option>');
+            }else{
+
+            }
+
+        })
+    });
+
+
+    function preencherDropdownSubcategoria(categoriaSelecionada) {
+        // $("#subcategorias").empty();
+
+        // $(categoriaSelecionada).each(function (index, subcategoria) {
+        //     console.log("subcategoria");
+        //     if (subcategoria.IdCategoriaPai == categoriaSelecionada) {
+        //         $("#subcategorias").append('<option value="' + subcategoria.Id + '">' + subcategoria.Nome + '</option>');
+        //     }
+        // });
+
+        // categoriaSelecionada.forEach(function () {
+
+        // })
+    }
+
+    $(document).ready(function () {
+        // $("#subcategorias").empty();
+
+    });
+
     return {
         init: init,
-        realizarSuporte: realizarSuporte
     }
 })()
