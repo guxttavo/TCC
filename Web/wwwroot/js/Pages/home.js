d3f7879c3512c089e008944877b4cd72c0136eaa
guxttavo <gustavowbarbosas@gmail.com>
2023-11-12 18:06:58 -0300

    estrutura inicial da home

---
+++ Web/wwwroot/js/Pages/home.js
@@ -0,0 +1,16 @@
+var home = (function () {
+    var configs = {
+        urls: {
+            buscarDados: ''
+        },
+    };
+
+    var init = function ($configs) {
+        configs = $configs;
+    };
+
+    return {
+        init: init,
+    }
+})()
+
