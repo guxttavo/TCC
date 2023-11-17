e0df0547d249552ff4ea315615b1f57c0ca87e5e
guxttavo <gustavowbarbosas@gmail.com>
2023-11-16 22:24:06 -0300

    grafico qtdDenuncias X Bairro funcionando

--- Web/wwwroot/js/Pages/graficos.js
+++ Web/wwwroot/js/Pages/graficos.js
@@ -1,45 +1,49 @@
 var graficos = (function () {
-    var configs = {
-        urls: {
-            buscarDados: ''
-        },
-    };
+  var configs = {
+    urls: {
+      buscarDados: ''
+    },
+  };
 
-    var init = function ($configs) {
-        configs = $configs;
-        $.get(configs.urls.buscarDados).done(function (data) {
-            console.log(data);
-            var bairros = [];
-            var categorias = [];
+  var init = function ($configs) {
+    configs = $configs;
+    $.get(configs.urls.buscarDados).done(function (data) {
+      // console.log(data);
+      var bairros = [];
+      data.forEach(function (data) {
+        bairros.push(data.nome);
+      });
 
-            data.forEach(function (data) {
-                bairros.push(data.bairro.nome);
-            })
+      var denuncias = [];
+      data.forEach(function (data) {
+        denuncias.push(data.denuncias);
+      })
 
-            data.forEach(function (data) {
-                categorias.push(data.categoria.nome);
-            })
-  
-              new Chart(
-                document.getElementById('acquisitions'),
-                {
-                  type: 'bar',
-                  data: {
-                    labels: bairros.map(row => row.nome),
-                    datasets: [
-                      {
-                        label: 'Denuncias por Bairro',
-                        data: data.map(row => row.count)
-                      }
-                    ]
-                  }
-                }
-              );
-        })
-    };
-    
-    return {
-        init: init,
-    }
+      var qtdDenuncias = [];
+      denuncias.forEach(function (subArray) {
+        qtdDenuncias.push(subArray.length)
+      });
+
+      new Chart(
+        document.getElementById('acquisitions'),
+        {
+          type: 'bar',
+          data: {
+            labels: bairros,
+            datasets: [
+              {
+                label: 'Denuncias por Bairro',
+                data: qtdDenuncias
+              }
+            ]
+          }
+        }
+      );
+    })
+  };
+
+  return {
+    init: init,
+  }
 })()
 
